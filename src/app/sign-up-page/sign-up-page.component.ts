28e44ac67fdd57e5153740cf5692f74febacf37a
Wizari <wizaripost@gmail.com>
2023-01-25 21:19:20 +0300

    sign-up-page

--- src/app/sign-up-page/sign-up-page.component.ts
+++ src/app/sign-up-page/sign-up-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-sign-up-page',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./sign-up-page.component.scss']
 })
 export class SignUpPageComponent {
+  signUpForm = new FormGroup({
+    login: new FormControl('i.simagin'),
+    email: new FormControl('i.simagin@mail.com'),
+    password1: new FormControl('123456'),
+    password2: new FormControl('123456')
+  });
+  // signUpForm = new FormGroup({
+  //   login: new FormControl('i.simagin'),
+  //   email: new FormControl('i.simagin@mail.com'),
+  //   password1: new FormControl('123456'),
+  //   password2: new FormControl('123456')
+  // });
 
+  onSubmit() {
+    console.log(this.signUpForm.value.login)
+    console.log(this.signUpForm.value.email)
+    console.log(this.signUpForm.value.password1)
+    console.log(this.signUpForm.value.password2)
+  }
 }
